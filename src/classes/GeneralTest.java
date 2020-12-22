e2cf9a7c49858ea62ab881f0ccb565f9728f90b7
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-22 11:33:19 +0100

    controls

--- src/classes/GeneralTest.java
+++ src/classes/GeneralTest.java
@@ -12,4 +12,24 @@ public boolean soustraction(int a,int b)
 return a>b;	
 
 }
+public boolean difference(int x,int y)
+{
+if(x==y)
+	
+	return false;
+else
+	return true;
+
+}
+public boolean paire(int p)
+{
+return p%2==0;	
+
+}
+public boolean verifChaine(String ch)
+{
+	return ch.isBlank();
+	
+
+}
 }
